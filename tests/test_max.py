dddedb8a52a24ef17ba262a2c491fb90be17e3b8
r4zewp <marijuanatoys@gmail.com>
2021-12-17 22:47:44 +0300

    changing tests

--- tests/test_max.py
+++ tests/test_max.py
@@ -2,6 +2,6 @@ import funcs_for_tests
 from datasets import *
 
 def test_on_first_input():
-    assert funcs_for_tests.n.maxOfList(list_1) == 33
+    assert funcs_for_tests.maxOfList(list_1) == 33
 def test_on_second_input():
     assert funcs_for_tests.maxOfList(list_2) == 55
