b1b3d5c4d4facbe9f60cb7da138e2bd5fa78228d
r4zewp <marijuanatoys@gmail.com>
2021-12-18 02:14:34 +0300

    testing tests

--- tests/test_max.py
+++ tests/test_max.py
@@ -1,7 +1,7 @@
-import funcs_for_test
+import funcs_for_tests
 from datasets import *
 
 def test_on_first_input():
-    assert funcs_for_tests.maxOfList(list_1) == 33
+    assert funcs_for_tests.maxOfList(list_1) == 32
 def test_on_second_input():
     assert funcs_for_tests.maxOfList(list_2) == 55
