c804b0af348d69312f3ae91917f21f260f80fc2a
r4zewp <marijuanatoys@gmail.com>
2021-12-18 06:28:28 +0300

    for vid

--- tests/test_file_valid.py
+++ tests/test_file_valid.py
@@ -5,4 +5,4 @@ def test_valid_file_reading_one():
     assert fileOpening("inputs/input_4.txt") == [1,1,1,1,2,3,4,5]
     
 def test_valid_file_reading_one():
-    assert fileOpening("inputs/input_5.txt") == [1,1,1,1,2,3,4,5,1,1,1,1,2,3,4,5,1,1,1,1,2,3,4,5]
+    assert fileOpening("inputs/input_5.txt") == [1,1,1,1,2,3,4,5,1,1,1,1,2,3,4,5,1,1,1,1,2,3,4,5,6]
